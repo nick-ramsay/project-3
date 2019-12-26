6d2fd7be3a5e54611d8b9dd2b8ede508d4a2a7ed
Nick Ramsay <NickRamsay1989@gmail.com>
2019-12-26 14:46:18 +1100

    Finished layout for Business page

--- client/src/components/UpdateAccountForm/UpdateAccountForm.js
+++ client/src/components/UpdateAccountForm/UpdateAccountForm.js
@@ -3,42 +3,72 @@ import "./style.css";
 
 function UpdateAccountForm(props) {
     return (
-                <div className="col-md-12">
-                    <form>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputPassword4">Phone</label>
-                                <input type="phone" class="form-control" id="inputPassword4" placeholder="Phone" />
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputAddress">Address</label>
-                            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
-                        </div>
-                        <div class="form-group">
-                            <label for="inputAddress2">Address 2</label>
-                            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <label for="inputCity">City</label>
-                                <input type="text" class="form-control" id="inputCity" />
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label for="inputState">State</label>
-                                <select id="inputState" class="form-control">
-                                    <option selected>Choose...</option>
-                                    <option>New South Wales</option>
-                                </select>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <label for="inputZip">Postcode</label>
-                                <input type="text" class="form-control" id="inputZip" />
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary mb-1">Create Account</button>
-                    </form>
+        <div className="col-md-12">
+            <form>
+                <h5 className="text-left">Contact Details</h5>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label for="businessName">Business Name</label>
+                        <input type="text" className="form-control" id="businessName" placeholder="Business Name" />
+                    </div>
                 </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label for="ownerName">Owner Name</label>
+                        <input type="text" className="form-control" id="ownerName" placeholder="Owner Name" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="businessPhone">Phone</label>
+                        <input type="phone" className="form-control" id="businessPhone" placeholder="Phone" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label for="businessEmail">Email</label>
+                        <input type="email" className="form-control" id="businessEmail" placeholder="Email" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label for="businessAddress1">Address</label>
+                    <input type="text" className="form-control" id="businessAddress1" placeholder="1234 Main St" />
+                </div>
+                <div className="form-group">
+                    <label for="businessAddress2">Address 2</label>
+                    <input type="text" className="form-control" id="businessAddress2" placeholder="Apartment, studio, or floor" />
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="businessCity">City</label>
+                        <input type="text" className="form-control" id="businessCity" />
+                    </div>
+                    <div className="form-group col-md-4">
+                        <label for="businessState">State</label>
+                        <select id="businessState" className="form-control">
+                            <option selected>Choose state...</option>
+                            <option>New South Wales</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label for="businessPostcode">Postcode</label>
+                        <input type="text" className="form-control" id="businessPostcode" placeholder="Postcode" />
+                    </div>
+                </div>
+                <h5 className="text-left">Service Details</h5>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="businessRate">Standard Hourly Rate</label>
+                        <input type="number" placeholder="10.00" step="0.01" min="0" className="form-control" id="businessRate" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label for="businessSpecialty">Specialties</label>
+                        <select id="businessSpecialty" className="custom-select" multiple>
+                            <option>Carpentry</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary mb-1 float-right" id="updateAccountBtn">Update Account</button>
+            </form>
+        </div>
     )
 }
 
