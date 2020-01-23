be0d8b77b68cebbaf6ef4d0f14b9288ce9d9c1af
Nick Ramsay <48420063+nick-ramsay@users.noreply.github.com>
2020-01-24 09:22:37 +1100

    Removed delete button from project list, serves no purpose

--- client/src/components/ProjectList/ProjectList.js
+++ client/src/components/ProjectList/ProjectList.js
@@ -12,7 +12,6 @@ function ProjectList(props) {
                 <div className="card-body">
                     <button className="btn btn-dark m-1 projectDetails btn-sm" type="button" data-toggle="collapse" id={props.projectID && "project" + props.projectID} data-target={props.projectID && "#projectDetails" + props.projectID}>Details</button>
                     <button className="btn btn-primary btn-sm m-1 float-right" id="editInventoryBtn" data-toggle="modal" data-target="#editProjectModal" data-project-state-index={props.projectStateIndex} onClick={props.editProject}><img src={require("../../images/edit-icon.png")} alt="Edit Project" /></button>
-                    <button className="btn btn-danger btn-sm m-1 float-right" id="deleteInventoryBtn"><img src={require("../../images/delete-icon.png")} alt="Delete Project" /></button>
                     <div id={props.projectID && "projectDetails" + props.projectID} class="collapse" aria-labelledby="headingOne" data-parent={props.projectID && "#project" + props.projectID}>
                         <div class="card-body">
                             <p>Project: {props.projectName}</p>
